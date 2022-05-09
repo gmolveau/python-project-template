6fde42910faa0e2284b13845ab5cdd986053c8c2
gmolveau <2090495+gmolveau@users.noreply.github.com>
2022-05-09 10:43:40 +0200

    add script to download offline dependencies

---
+++ docs/offline.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+t=$(mktemp -d /tmp/pypi_XXXX)
+echo "workdir = $t"
+cd "$t" || exit
+python3 -m pip download \
+    poetry \
+    black \
+    pylint \
+    mypy \
+    isort \
+    bandit \
+    pytest \
+    pydocstyle \
+    flake8 \
+    structlog \
+    memray \
+    sqltap \
+    pyinstrument \
+    sqlalchemy \
+    alembic \
+    typer \
+    click \
+    fastapi \
+    flask \
+    pydantic \
+    marshmallow \
+    python-dotenv \
+    gunicorn
+zip -r "$t.zip" "$t/"
+echo "done : $t.zip"
+# twine upload --skip-existing *
